5a40c74a174d22955ed6d9b077ce3ac54efade59
Unicornff <2011629480@qq.com>
2025-05-06 16:04:32 +0800

    Updates

--- assets/TestCode-JKWkYTkf.js
+++ assets/TestCode-JKWkYTkf.js
@@ -1,4 +1,4 @@
-import{P as l}from"./prism-tomorrow-CBUOVrnP.js";import{a as i,k as p,n as a,o as m,c as d,h as n,q as o}from"./index-CZgHeNru.js";const u={class:"code-class"},g={class:"line-numbers"},f={class:"language-html"},j=["innerHTML"],M=i({__name:"TestCode",setup(v){const r=p(`
+import{P as l}from"./prism-tomorrow-DULZKHYe.js";import{a as i,k as p,n as a,o as m,c as d,h as n,q as o}from"./index-tOJRS_5n.js";const u={class:"code-class"},g={class:"line-numbers"},f={class:"language-html"},j=["innerHTML"],M=i({__name:"TestCode",setup(v){const r=p(`
 <div>Example</div>
 <div>Hello</div>
 `);a(()=>{l.highlightAll()});const e=t=>t.replace(/<[^>]*>/g,function(s){return s.replace(/</g,"&lt;").replace(/>/g,"&gt;")});return(t,s)=>(m(),d("div",u,[s[4]||(s[4]=n("h2",null,"页面代码区（使用prismjs）",-1)),s[5]||(s[5]=n("br",null,null,-1)),s[6]||(s[6]=n("h3",null,"html",-1)),n("pre",g,[s[2]||(s[2]=o("			")),n("code",f,[s[0]||(s[0]=o(`
